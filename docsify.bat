d7d7c758a0c82eed483d2bed9273ec95535e2193
codeMegan <lyjs0012@hotmail.com>
2022-02-21 01:54:15 +0800

    myblogs first commit

---
+++ docsify.bat
@@ -0,0 +1,8 @@
+git init
+git pull origin master
+git add -A 
+git commit -m "myblogs first commit"
+git remote rm origin
+git remote add origin https://github.com/codeMegan/docsify.git
+git push -f origin master
+
